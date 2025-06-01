36fba3248ee66da42b59be5c28afe85121acc108
Sandip Kumar Yadav <sandipkumaryadav@192.168.1.5>
2025-06-01 16:10:59 +0530

    fixing build issues

---
+++ build/precache-manifest.e9bb5a1f5caeb8460453f7736ae824aa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68c92027a1bf2a7b57d27d4b062d63ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7485dd18b3992f39f72a",
+    "url": "/static/css/main.f47b8dab.chunk.css"
+  },
+  {
+    "revision": "a129853df2fcca909ced",
+    "url": "/static/js/2.3c6ed3d6.chunk.js"
+  },
+  {
+    "revision": "1eb98706859bae847c8762bbc3c10000",
+    "url": "/static/js/2.3c6ed3d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6b4f7f47678b23db0f4",
+    "url": "/static/js/3.b84af2c6.chunk.js"
+  },
+  {
+    "revision": "7485dd18b3992f39f72a",
+    "url": "/static/js/main.2b41be64.chunk.js"
+  },
+  {
+    "revision": "a025798708c6c9e27b65",
+    "url": "/static/js/runtime-main.99a913f2.js"
+  },
+  {
+    "revision": "76289e56c355e35b9264f9b663302f50",
+    "url": "/static/media/iskcon-logo.76289e56.png"
+  }
+]);
