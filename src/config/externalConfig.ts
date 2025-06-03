294be2b718e0ac3164f7ee57c82a76a6c727ad1a
sundaragopesvaradas-jps <sundaragopesvaradas.jps@gmail.com>
2025-06-03 19:02:19 +0530

    add new link for SP letters notebook lm link (#3)
    * Update routes.ts
    
    * Update externalConfig.ts
    
    * Update Footer.tsx

--- src/config/externalConfig.ts
+++ src/config/externalConfig.ts
@@ -1,8 +1,14 @@
 export const externalConfig = {
-    askQuestions: {
-        text: 'Ask any questions based on Prabhupada Books (AI)',
+    askQuestionsBooks: {
+        text: 'Ask any questions based on Srila Prabhupada Books (AI)',
         url: 'https://notebooklm.google.com/notebook/3ce18730-fc6c-40a0-bc93-1cb531521b7f',
         icon: 'fas fa-book-reader'
     },
+    askQuestionsLetters: {
+        text: 'Ask any questions based on Srila Prabhupada Conversations, Letters, Lectures (AI)',
+        url: 'https://notebooklm.google.com/notebook/7a13c623-df4b-440b-b10f-2407c3ab8b6a',
+        icon: 'fas fa-book-reader'
+    },
+    
     showGallerySections: false
 }; 
--- src/config/routes.ts
+++ src/config/routes.ts
@@ -1,9 +1,11 @@
 export const routes = {
   home: '/home',
-  spllm: '/spllm',
+  spbooks: '/spbooks',
+  spletters: '/spletters',
   default: '/home',
   external: {
-    spllm: 'https://notebooklm.google.com/notebook/3ce18730-fc6c-40a0-bc93-1cb531521b7f'  // www.iskcondeoghar.in/spllm redirects to this URL
+    spbooks: 'https://notebooklm.google.com/notebook/3ce18730-fc6c-40a0-bc93-1cb531521b7f',
+    spletters: 'https://notebooklm.google.com/notebook/7a13c623-df4b-440b-b10f-2407c3ab8b6a'
   }
 } as const;
 
