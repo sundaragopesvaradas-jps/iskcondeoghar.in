e4e55fcb9697a9b1970623b87095f1f2c42505e8
Sandip Kumar Yadav <sandipkumaryadav@Sandips-MacBook-Air.local>
2025-06-21 23:13:05 +0530

    Revert "Update App.tsx for ip address logging into table"
    This reverts commit 1361049ffacad042af28236fb0bf983651fe9eb9.

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,8 @@ import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import EventGallery from './components/EventGallery';
-import { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
-
-function useLogVisit() {
-  const location = useLocation();
-  useEffect(() => {
-    fetch('/api/logVisit', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ path: location.pathname })
-    });
-  }, [location]);
-}
 
 function App() {
-  useLogVisit();
   return (
     <div className="App">
       <Header />
