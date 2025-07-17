78cc3cce7da93ed7cba9473afe9f9586bbb9382d
sundaragopesvaradas-jps <sundaragopesvaradas.jps@gmail.com>
2025-07-17 12:27:03 +0530

    Added Janmashtami 2025 Banner And Details
    * Update upcomingFestival.ts
    
    * Add files via upload

--- src/data/upcomingFestival.ts
+++ src/data/upcomingFestival.ts
@@ -1,9 +1,10 @@
 import { UpcomingFestival } from './types';
 
 export const upcomingFestival: UpcomingFestival = {
-  title: "Rath Yatra 2025",
-  date: "2025-06-27",
-  time: "10:00", // 10:00 AM IST
-  image: "/images/UpcomingEventBanner/event-photo.jpg",
-  description: "The auspicious Snana Yatra ceremony is on June 11, 2025. Join us for the grand celebration of Rath Yatra, the Festival of Chariots, featuring Lord Jagannath, Lord Balabhadra, and Devi Subhadra."
+  title: "Sri Krishna Janmashtami 2025",
+  date: "2025-08-16",
+  time: "18:00", // 10:00 AM IST
+  image: "/images/UpcomingEventBanner/Janmashtami2025.jpg",
+  description: "Celebrate the appearance of Lord Sri Krishna with us this Janmashtami! Join us for a divine Maha Yagya at 10am, vibrant cultural program and Maha abhisheka from 6pm, and the joyous Nandotsava the next day at 11am. Don't miss this spiritual and cultural extravaganza"
+  //description: "The auspicious Snana Yatra ceremony is on June 11, 2025. Join us for the grand celebration of Rath Yatra, the Festival of Chariots, featuring Lord Jagannath, Lord Balabhadra, and Devi Subhadra."
 }; 
