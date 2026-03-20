5aa64f99610e4c594d19a1e55054d11788082566
Sandip Kumar Yadav <sandipkumar.yadav@salesforce.com>
2026-03-20 20:16:14 +0530

    Updating Upcoming Festival Page

--- src/data/upcomingFestival.ts
+++ src/data/upcomingFestival.ts
@@ -1,10 +1,10 @@
 import { UpcomingFestival } from './types';
 
 export const upcomingFestival: UpcomingFestival = {
-  title: "Sri Radhashtami 2025",
-  date: "2025-08-31",
-  time: "10:00", // 10:00 AM IST
-  image: "/images/UpcomingEventBanner/Janmashtami2025.jpg",
-  description: "Celebrate the divine appearance of Srimati Radharani with us this Radhashtami! The festivities begin with Mangal Arati at 6am, followed by a spiritual program starting at 9am featuring enlightening Katha, a grand Maha Abhisheka, and a vibrant Maha Arati. Conclude the celebration with a sumptuous Mahaprasadam feast. Join us for a day filled with devotion, culture, and joy!"
-  //description: "The auspicious Snana Yatra ceremony is on June 11, 2025. Join us for the grand celebration of Rath Yatra, the Festival of Chariots, featuring Lord Jagannath, Lord Balabhadra, and Devi Subhadra."
+  title: "Snana Yatra and Ratha Yatra 2026",
+  date: "2026-06-29",
+  time: "11:30", // 11:30 AM IST
+  image: "/images/UpcomingEventBanner/event-photo.jpg",
+  //description: "Celebrate the divine appearance of Srimati Radharani with us this Radhashtami! The festivities begin with Mangal Arati at 6am, followed by a spiritual program starting at 9am featuring enlightening Katha, a grand Maha Abhisheka, and a vibrant Maha Arati. Conclude the celebration with a sumptuous Mahaprasadam feast. Join us for a day filled with devotion, culture, and joy!"
+  description: "The auspicious Snana Yatra ceremony is on June 29, 2025. Join us for the grand celebration of Rath Yatra, the Festival of Chariots for the divine pleasure of Lord Jagannath Balabhadra and Subhadra Maharani."
 }; 
