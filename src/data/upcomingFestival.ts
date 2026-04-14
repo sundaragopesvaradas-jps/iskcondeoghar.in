e2ee52ee73df459a4f78b0dd48ab5b6b499f0b1c
sundaragopesvaradas-jps <sundaragopesvaradas.jps@gmail.com>
2026-04-14 20:44:01 +0530

    Update upcomingFestival.ts

--- src/data/upcomingFestival.ts
+++ src/data/upcomingFestival.ts
@@ -6,5 +6,5 @@ export const upcomingFestival: UpcomingFestival = {
   time: "11:30", // 11:30 AM IST
   image: "/images/UpcomingEventBanner/event-photo.jpg",
   //description: "Celebrate the divine appearance of Srimati Radharani with us this Radhashtami! The festivities begin with Mangal Arati at 6am, followed by a spiritual program starting at 9am featuring enlightening Katha, a grand Maha Abhisheka, and a vibrant Maha Arati. Conclude the celebration with a sumptuous Mahaprasadam feast. Join us for a day filled with devotion, culture, and joy!"
-  description: "The auspicious Snana Yatra ceremony is on June 29, 2025. Join us for the grand celebration of Rath Yatra, the Festival of Chariots for the divine pleasure of Lord Jagannath Balabhadra and Subhadra Maharani."
+  description: "The auspicious Snana Yatra ceremony is on June 29, 2026. Join us for the grand celebration of Rath Yatra, the Festival of Chariots for the divine pleasure of Lord Jagannath Balabhadra and Subhadra Maharani."
 }; 
