b172b9a05f766c3a64e112790f5dfb9e5bd705ed
sundaragopesvaradas-jps <sundaragopesvaradas.jps@gmail.com>
2026-07-28 22:59:43 +0530

    Update upcomingFestival.ts

--- src/data/upcomingFestival.ts
+++ src/data/upcomingFestival.ts
@@ -2,8 +2,8 @@ import { UpcomingFestival } from './types';
 
 export const upcomingFestival: UpcomingFestival = {
   title: "Sri Krishna Janmashtami 2026",
-  date: "2026-09-05",
-  time: "18:00", // 6:00 PM IST
+  date: "2026-09-04",
+  time: "17:00", // 6:00 PM IST
   image: "/images/UpcomingEventBanner/Janmashtami2025.jpg",
   description: "Celebrate the appearance of Lord Sri Krishna with us this Janmashtami! Join us for a divine Maha Yagya at 10am, vibrant cultural program and Maha abhisheka from 6pm, and the joyous Nandotsava the next day at 11am. Don't miss this spiritual and cultural extravaganza"
   //description: "Join us for Ratha Yatra 2026 at ISKCON Deoghar on 16th July at 12 PM. Be part of the devotional chariot procession of Lord Jagannath, Baladeva and Subhadra Maharani."
